c9cd3e3a523d4070c88f126857dd05d86e51a44b
daeclan <declxn@gmail.com>
2020-07-16 17:10:15 -0500

    Added the update and destroy functions.

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -12,6 +12,7 @@ class Api::ContactsController < ApplicationController
       last_name: params[:last_name],
       email: params[:email],
       phone_number: params[:phone_number],
+      user_id: current_user.id,
     )
     @contact.save
     render "show.json.jb"
@@ -23,5 +24,18 @@ class Api::ContactsController < ApplicationController
   end
 
   def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.first_name = params[:first_name] || @contact.first_name,
+                          @contact.last_name = params[:last_name] || @contact.last_name,
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number,
+    @receipe.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+    render json: { message: "You have removed the contact." }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     get "/contacts/index" => "contacts#index"
     post "/contacts" => "contacts#create"
     get "/contacts/:id" => "contacts#show"
+    patch "contacts/:id" => "contacts#update"
+    delete "contacts/:id" => "contacts#destroy"
+
     post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
   end
 end
